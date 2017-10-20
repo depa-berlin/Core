0f1e71a18037dbcf0ca6d2439855c4165638f0ae
Mirko Fenrich <fenrich@designpark.de>
2017-10-20 16:14:13 +0200

    ActiveRecordPaginator integriert

---
+++ src/DataModel/ActiveRecord/ActiveRecordPaginator.php
@@ -0,0 +1,86 @@
+<?php
+namespace Depa\Core\DataModel\ActiveRecord;
+
+use Depa\Core\DataModel\ActiveRecord\ActiveRecord;
+use Depa\Core\DataModel\ActiveRecord\Adapter\ActiveRecordAdapter;
+use Depa\Core\Api\Hal;
+use Zend\Paginator\Paginator;
+use Zend\Diactoros\Uri;
+
+/**
+ *
+ * @author fenrich
+ *        
+ */
+class ActiveRecordPaginator extends Paginator
+{
+
+    protected static $defaultItemCountPerPage = 1;
+
+    /**
+     * Constructor.
+     *
+     * @param AdapterInterface|AdapterAggregateInterface $adapter            
+     * @throws Exception\InvalidArgumentException
+     */
+    public function __construct(ActiveRecord $activeRecord)
+    {
+        $adapter = new ActiveRecordAdapter($activeRecord);
+        parent::__construct($adapter);
+    }
+
+    public function toHal(Uri $requestUri)
+    {
+        $apiHal = new Hal();
+        $apiHal->addElement('count', $this->getTotalItemCount());
+        $this->_makeLink($apiHal, $requestUri);
+        
+        foreach ($this->getCurrentItems() as $record) {
+            
+            // Query aus Uri entfernen
+            $requestUri = $requestUri->withQuery("");
+            // Path mit ID ergänzen
+            $path = $requestUri->getPath();
+            $requestUri = $requestUri->withPath($path . "/" . $record->id);
+            
+            $apiHal->addEmbed((new \ReflectionClass($record))->getShortName(), $record->toHal($requestUri));
+        }
+        
+        return $apiHal->getHal();
+    }
+
+    protected function _makeLink($apiHal, Uri $requestUri)
+    {
+        $queryArray = explode('&', $requestUri->getQuery());
+        
+        // page Information aus QueryString entfernen
+        $newQueryArray = array();
+        foreach ($queryArray as $key => $value) {
+            if (stripos($value, 'page') === FALSE) {
+                $newQueryArray[] = $value;
+            }
+        }
+        
+        $currentPageNumber = $this->getCurrentPageNumber();
+        
+        $pageCount = $this->count();
+        $apiHal->addLink('self', $this->_makeUri($requestUri, $newQueryArray, 'page=' . $currentPageNumber));
+        $apiHal->addLink('first', $this->_makeUri($requestUri, $newQueryArray, 'page=1'));
+        $apiHal->addLink('last', $this->_makeUri($requestUri, $newQueryArray, '?page=' . $pageCount));
+        // Previous and next
+        if ($currentPageNumber - 1 > 0) {
+            $apiHal->addLink('prev', $this->_makeUri($requestUri, $newQueryArray, '?page=' . ($currentPageNumber - 1)));
+        }
+        
+        if ($currentPageNumber + 1 <= $pageCount) {
+            $apiHal->addLink('next', $this->_makeUri($requestUri, $newQueryArray, '?page=' . ($currentPageNumber + 1)));
+        }
+    }
+
+    protected function _makeUri(Uri $uri, $queryArray, $newQuery)
+    {
+        $queryArray[] = $newQuery;
+        return $uri->withQuery(implode("&", $queryArray));
+    }
+}
+
---
+++ src/DataModel/ActiveRecord/Adapter/ActiveRecordAdapter.php
@@ -0,0 +1,58 @@
+<?php
+namespace Depa\Core\DataModel\ActiveRecord\Adapter;
+
+use Zend\Paginator\Adapter\AdapterInterface;
+use Depa\Core\DataModel\ActiveRecord\ActiveRecord;
+
+/**
+ *
+ * @author fenrich
+ *        
+ */
+class ActiveRecordAdapter implements AdapterInterface
+{
+
+    /**
+     *
+     * @var \Core\Model\ActiveRecord
+     */
+    protected $activeRecord;
+
+    public function __construct(ActiveRecord $activeRecord, $conditions = '')
+    {
+        $this->activeRecord = $activeRecord;
+        $this->conditions = $conditions;
+    }
+
+    /**
+     * Returns an array of items for a page.
+     *
+     * @param int $offset
+     *            Page offset
+     * @param int $itemCountPerPage
+     *            Number of items per page
+     * @return array
+     */
+    public function getItems($offset, $itemCountPerPage)
+    {
+        
+        // Resultset von ActiveRecord holen
+        // offset ist das errechnete Element wo ich beginne (Seitenzahl * element je seite)
+        $resultSet = forward_static_call([$this->activeClass,'getRecords'], $offset, $itemCountPerPage);
+        //muss iterator to array sein?
+        return iterator_to_array($resultSet);
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see Countable::count()
+     *
+     */
+    public function count()
+    {
+        // Gesamtzahl der Elemente in DB
+        return (forward_static_call([$this->activeClass,'getRecordCount'], $this->condition));
+    }
+}
+
